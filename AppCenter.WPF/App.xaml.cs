274df790f57e5a85269ae1ba11bcc22906a65a72
Kazuki Ota <kaota@microsoft.com>
2020-04-02 12:12:00 +0900

    Add project files.

---
+++ AppCenter.WPF/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AppCenter.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Microsoft.AppCenter.AppCenter.Start(
+                "27da81b6-9612-48d6-a1b0-320133f2c159",
+                typeof(Analytics), 
+                typeof(Crashes));
+        }
+    }
+}
